b8277f5f54f06d5f0787f0d6666d08c04fce9533
euchrisgab <christofergabriel06@gmail.com>
2026-09-10 13:20:48 +0000

    aula4

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+(const alternativa of perguntaAtual.alternativas)
 
   const perguntas = [
   {
@@ -42,5 +43,21 @@ const textoResultado = document.querySelector(".texto-resultado");
     ],
   },
 ];
+
 let atual = 0;
 let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+mostraPergunta();
